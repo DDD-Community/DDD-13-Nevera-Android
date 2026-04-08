a6a072dc2005ac5fae51dd7f587a74de742cac43
jackson <mns0304@gmail.com>
2026-04-08 18:29:40 +0900

    refactor: NetworkError 하위 타입의 throwable을 상위 프로퍼티로 통합

--- core/common/src/main/kotlin/com/anddd/nevera/core/common/NetworkError.kt
+++ core/common/src/main/kotlin/com/anddd/nevera/core/common/NetworkError.kt
@@ -3,6 +3,7 @@ package com.anddd.nevera.core.common
 sealed interface NetworkError {
     val code: Int?
     val message: String?
+    val throwable: Throwable?
 
     /**
      * Server Status Code 참조
@@ -10,24 +11,24 @@ sealed interface NetworkError {
     data class HttpError(
         override val code: Int,
         override val message: String? = null,
-        val throwable: Throwable? = null
+        override val throwable: Throwable? = null
     ) : NetworkError
 
     data class NetworkConnectionError(
         override val code: Int? = null,
         override val message: String? = "네트워크 연결 오류",
-        val throwable: Throwable? = null
+        override val throwable: Throwable? = null
     ) : NetworkError
 
     data class TimeoutError(
         override val code: Int? = null,
         override val message: String? = "요청 시간이 초과되었습니다.",
-        val throwable: Throwable? = null
+        override val throwable: Throwable? = null
     ) : NetworkError
 
     data class UnknownError(
         override val code: Int? = null,
         override val message: String? = "알 수 없는 오류가 발생했습니다.",
-        val throwable: Throwable? = null
+        override val throwable: Throwable? = null
     ) : NetworkError
 }
