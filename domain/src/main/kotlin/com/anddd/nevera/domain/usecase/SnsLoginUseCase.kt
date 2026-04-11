7c9edc23aded1afee1d618bc93652b1a3f7a02de
jackson <mns0304@gmail.com>
2026-04-11 21:39:35 +0900

    feat(domain): GoogleLoginUseCase 추가 및 SnsLoginUseCase deprecated 처리

--- domain/src/main/kotlin/com/anddd/nevera/domain/usecase/SnsLoginUseCase.kt
+++ domain/src/main/kotlin/com/anddd/nevera/domain/usecase/SnsLoginUseCase.kt
@@ -1,26 +1,4 @@
 package com.anddd.nevera.domain.usecase
 
-import com.anddd.nevera.core.common.ApiResult
-import com.anddd.nevera.domain.model.LoginResult
-import com.anddd.nevera.domain.model.LoginProvider
-import com.anddd.nevera.domain.repository.UserRepository
-import com.anddd.nevera.domain.repository.TokenRepository
-import javax.inject.Inject
-
-class SnsLoginUseCase @Inject constructor(
-    private val userRepository: UserRepository,
-    private val tokenRepository: TokenRepository
-) {
-
-    suspend operator fun invoke(token: String): ApiResult<LoginResult> {
-        val result = userRepository.loginWithGoogle(token)
-        if (result is ApiResult.Success) {
-            tokenRepository.setLoginInfo(
-                accessToken = result.data.accessToken,
-                refreshToken = result.data.refreshToken,
-                provider = LoginProvider.GOOGLE
-            )
-        }
-        return result
-    }
-}
+@Deprecated("Use GoogleLoginUseCase instead", ReplaceWith("GoogleLoginUseCase"))
+typealias SnsLoginUseCase = GoogleLoginUseCase
