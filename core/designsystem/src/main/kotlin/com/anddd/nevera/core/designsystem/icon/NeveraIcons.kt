79eb9d4883317ded0829b69c8c1d865d048d44b3
Ju Hyeok <zero5.two4@gmail.com>
2026-04-26 10:07:21 +0900

    feat(designsystem): NeveraIcons 추가 및 AppBar용 아이콘 리소스 5종 추가
     - ArrowBack, Close, Info, Menu, Search 벡터 드로어블 추가
     - Compose Painter 접근자를 제공하는 NeveraIcons 객체 추가

---
+++ core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/icon/NeveraIcons.kt
@@ -0,0 +1,19 @@
+package com.anddd.nevera.core.designsystem.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.anddd.nevera.core.designsystem.R
+
+object NeveraIcons {
+    val ArrowBack: Painter
+        @Composable get() = painterResource(R.drawable.ic_arrow_back)
+    val Close: Painter
+        @Composable get() = painterResource(R.drawable.ic_close)
+    val Info: Painter
+        @Composable get() = painterResource(R.drawable.ic_info)
+    val Menu: Painter
+        @Composable get() = painterResource(R.drawable.ic_menu)
+    val Search: Painter
+        @Composable get() = painterResource(R.drawable.ic_search)
+}
