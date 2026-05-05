f1bc4eacf2e7f51d16553bcc40d0b1ca75bd4eed
jackson <mns0304@gmail.com>
2026-05-05 22:04:04 +0900

    design(designsystem): BottomSheet 헤더 레이아웃과 Preview 정리

--- core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/component/bottomsheet/NeveraConfirmBottomSheet.kt
+++ core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/component/bottomsheet/NeveraConfirmBottomSheet.kt
@@ -2,14 +2,11 @@ package com.anddd.nevera.core.designsystem.component.bottomsheet
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.anddd.nevera.core.designsystem.component.bottomsheet.internal.NeveraBottomSheet
 import com.anddd.nevera.core.designsystem.component.confirm.internal.ConfirmActionButtons
 import com.anddd.nevera.core.designsystem.component.confirm.internal.ConfirmTitleContent
-import com.anddd.nevera.core.designsystem.ui.theme.NeveraTheme
 
 /**
  * 제목, 설명, 확인/취소 액션을 가진 BottomSheet 조합 컴포넌트입니다.
@@ -38,23 +35,6 @@ fun NeveraConfirmBottomSheet(
             negative = negative,
             onPositive = onPositive,
             onNegative = onNegative,
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
-@Composable
-private fun NeveraConfirmBottomSheetPreview() {
-    NeveraTheme {
-        NeveraConfirmBottomSheet(
-            title = "Title",
-            subtitle = "Subtitle",
-            positive = "CTA",
-            negative = "Sub",
-            onPositive = {},
-            onNegative = {},
-            sheetState = rememberModalBottomSheetState(),
         )
     }
 }
