f0addc43e9deca2417db335491416ecf87d79b36
Ju Hyeok <zero5.two4@gmail.com>
2026-05-06 17:26:09 +0900

    refactor(mypage): SettingsContent의 LazyColumn을 Column으로 교체 및 contentDescription 개선
     - 고정 목록이므로 LazyColumn 불필요, Column + forEach로 단순화
     - 장식용 아이콘의 contentDescription을 null로 수정

--- feature/mypage/src/main/kotlin/com/anddd/nevera/feature/mypage/main/component/SettingsContent.kt
+++ feature/mypage/src/main/kotlin/com/anddd/nevera/feature/mypage/main/component/SettingsContent.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -43,12 +41,8 @@ internal fun SettingsContent(
 
         Spacer(modifier = Modifier.height(NeveraTheme.spacing.gap8))
 
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            items(
-                items = settingItems, key = { it.type }
-            ) { item ->
+        Column(modifier = Modifier.fillMaxWidth()) {
+            settingItems.forEach { item ->
                 SettingItem(
                     item = item,
                     onClick = { onClick(item.type) }
@@ -93,7 +87,7 @@ private fun SettingItem(
         Image(
             modifier = Modifier.size(NeveraTheme.iconSize.xSmall),
             painter = painterResource(R.drawable.ic_chevron_right),
-            contentDescription = "Right Arrow",
+            contentDescription = null,
         )
     }
 }
