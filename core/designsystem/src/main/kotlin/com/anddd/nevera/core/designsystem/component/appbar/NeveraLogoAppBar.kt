c3e44d40eb01c0aea8202b6484c63cfca98293d4
Ju Hyeok <zero5.two4@gmail.com>
2026-05-20 19:14:45 +0900

    fix(logo-app-bar): 미사용 Import 제거

--- core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/component/appbar/NeveraLogoAppBar.kt
+++ core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/component/appbar/NeveraLogoAppBar.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.anddd.nevera.core.designsystem.icon.NeveraIcons
 import com.anddd.nevera.core.designsystem.ui.theme.NeveraTheme
