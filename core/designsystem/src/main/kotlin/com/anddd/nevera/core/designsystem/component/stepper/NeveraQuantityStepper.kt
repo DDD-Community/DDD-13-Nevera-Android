52dc87c8f156052d268e53107c91f0489d327bba
jackson <mns0304@gmail.com>
2026-05-23 01:05:57 +0900

    style(designsystem): NeveraQuantityStepper 미사용 import 제거

--- core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/component/stepper/NeveraQuantityStepper.kt
+++ core/designsystem/src/main/kotlin/com/anddd/nevera/core/designsystem/component/stepper/NeveraQuantityStepper.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
@@ -18,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
