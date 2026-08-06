05fcae6feacf74e72ec6f33fa6b6503cb86410c0
Ju Hyeok <zero5.two4@gmail.com>
2026-08-06 22:10:35 +0900

    chore(app): 앱 버전 1.0.4로 상향

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
 
     defaultConfig {
         applicationId = "com.anddd.nevera"
-        versionCode = 7
-        versionName = "1.0.3"
+        versionCode = 8
+        versionName = "1.0.4"
     }
 
     signingConfigs {
